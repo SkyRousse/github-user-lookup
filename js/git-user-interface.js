25ec9f0bb3c6a10088d9c2fef07ee9e42151ecb7
sr <skyrousse@gmail.com>
2016-08-05 12:34:31 -0700

    refine prototype and display methods for finding and displaying user info

--- js/git-user-interface.js
+++ js/git-user-interface.js
@@ -4,17 +4,28 @@ var displayRepos = function(repoName, repoDescription) {
   $('.show-repos').append('<tr> + <td>' + repoName + '</td> + <td>' + repoDescription + '</td><tr>')
 }
 
-// var displayUser = function(name, )
+var displayUser = function( name, email, followers, following, publicRepos, avatarUrl ) {
+  $('.show-user-info').html(
+    '<ul><li>' + name + '</li>' +
+    '<ul><li>' + email + '</li>' +
+    '<ul><li>' + followers + '</li>' +
+    '<ul><li>' + following + '</li>' +
+    '<ul><li>' + publicRepos + '</li>' +
+    '</ul>'
+  )
+}
 
 $(document).ready(function() {
   var userObjectSearch = new GitUser();
   $('#user-form').submit(function(event) {
     event.preventDefault();
     $('.show-repos').empty();
+    $('.show-user-info').empty();
     var userName = $('#user-name').val();
     $('#user-name').val("");
     userObjectSearch.getRepos(userName, displayRepos);
     $('.show-repos').show();
-    userObjectSearch.getUserInfo(userName/*, displayUser*/);
+    userObjectSearch.getUserInfo(userName, displayUser);
+    $('.show-user-info').show();
   });
 });
--- js/git-user.js
+++ js/git-user.js
@@ -4,21 +4,28 @@ function GitUser() {
 
 }
 
-GitUser.prototype.getUserInfo = function(userName/*, displayUser*/) {
+GitUser.prototype.getUserInfo = function(userName, displayUser) {
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response) {
-    console.log(response);
-  })
-}
+    var name = response.name;
+    var email = response.email;
+    var followers = response.followers;
+    var following = response.following;
+    var publicRepos = response.public_repos;
+    var avatarUrl = response.avatar_url;
+    displayUser(name, email, followers, following, publicRepos, avatarUrl);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+    $('.showRepos').text(error.responseJSON.message);
+  });
+};
 
-GitUser.prototype.getRepos = function(userName, displayFunction) {
+GitUser.prototype.getRepos = function(userName, displayRepos) {
   $.get('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response){
     var foundRepos = response;
     foundRepos.forEach(function(repo) {
       var repoName = repo.name;
       var repoDescription = repo.description;
-      console.log("Repo Name:" + repoName);
-      console.log("Description:" + repoDescription);
-      displayFunction(repoName, repoDescription);
+      displayRepos(repoName, repoDescription);
     })
   }).fail(function(error){
     console.log(error.responseJSON.message);
