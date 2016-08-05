d1836387007074bdbefa2c313c3fd68fc37881db
sr <skyrousse@gmail.com>
2016-08-05 15:48:12 -0700

    add styling with sass mixins and flexbox, fix bug that breaks flexbox by removing display none from css

--- js/git-user.js
+++ js/git-user.js
@@ -15,7 +15,7 @@ GitUser.prototype.getUserInfo = function(userName, displayUser) {
     displayUser(name, email, followers, following, publicRepos, avatarUrl);
   }).fail(function(error){
     console.log(error.responseJSON.message);
-    $('.showRepos').text(error.responseJSON.message);
+    $('#showRepos').text(error.responseJSON.message);
   });
 };
 
@@ -30,7 +30,7 @@ GitUser.prototype.getRepos = function(userName, displayRepos) {
     })
   }).fail(function(error){
     console.log(error.responseJSON.message);
-    $('.showRepos').text(error.responseJSON.message);
+    $('#showRepos').text(error.responseJSON.message);
   });
 };
 
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,32 +1,31 @@
 var GitUser = require('./../js/git-user.js').gituserModule;
 
 var displayRepos = function(repoName, repoDescription) {
-  $('.show-repos').append("<div class='item'>" + "<h4>" + repoName + "</h4><p>" + repoDescription + "</p></div>")
-}
+  $('#show-repos').append("<div class='item'>" + "<h4>" + repoName + "</h4><p>" + repoDescription + "</p></div>");
+};
 
 var displayUser = function( name, email, followers, following, publicRepos, avatarUrl ) {
-  $('.show-user-info').html(
-    "<div class='avatar'> <img src='" + avatarUrl + "'>" +
+  $('#show-user-info').html(
+    "<div class='top-item' >" +
+    "<img src='" + avatarUrl + "'>" +
     '<ul><li>' + name + '</li>' +
     '<li>' + email + '</li>' +
-    '<li>' + followers + '</li>' +
-    '<li>' + following + '</li>' +
-    '<li>' + publicRepos + '</li>' +
-    '</ul>'
-  )
-}
+    '<li>' + 'Followers: ' + followers + '</li>' +
+    '<li>' + 'Following: '+ following + '</li>' +
+    '<li>' + 'Public Repos: '+ publicRepos + '</li>' +
+    '</ul></div>'
+  );
+};
 
 $(document).ready(function() {
   var userObjectSearch = new GitUser();
   $('#user-form').submit(function(event) {
     event.preventDefault();
-    $('.show-repos').empty();
-    $('.show-user-info').empty();
+    $('#show-repos').empty();
+    $('#show-user-info').empty();
     var userName = $('#user-name').val();
     $('#user-name').val("");
     userObjectSearch.getRepos(userName, displayRepos);
-    $('.show-repos').show();
     userObjectSearch.getUserInfo(userName, displayUser);
-    $('.show-user-info').show();
   });
 });
