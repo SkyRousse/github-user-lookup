9447ff13c5e6ffffef840cea2df5148ea8a9e130
sr <skyrousse@gmail.com>
2016-08-04 23:51:28 -0700

    add completed gulpfile

--- js/git-user.js
+++ js/git-user.js
@@ -7,6 +7,7 @@ function GitUser() {
 GitUser.prototype.exports.getRepos = function(userName){
   $.get('https://api.github.com/users/' + 'userName' + '?access_token=' + apiKey).then(function(response){
     console.log(response);
+    // displayFunction(userName, response.dig-into-api);
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
