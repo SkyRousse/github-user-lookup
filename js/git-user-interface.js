9a3ed3c4f80f9a81bd01d3ca963d4b7d780936d5
sr <skyrousse@gmail.com>
2016-08-04 22:36:30 -0700

    initial site setup with directories and core files

---
+++ js/git-user-interface.js
@@ -0,0 +1,11 @@
+var GitUser = require('./../js/git-user.js').gituserModule;
+
+$(document).ready(function() {
+  var currentUserObject = new GitUser();
+  $('#user-form').submit(function(event) {
+    event.preventDefault();
+    var userName = $('#user-name').val();
+    $('#user-name').val("");
+    currentUserObject.findUser(userName);
+  });
+});
---
+++ js/git-user.js
@@ -0,0 +1,15 @@
+var apiKey = require('./../.env').apiKey;
+
+function GitUser() {
+
+}
+
+GitUser.prototype.exports.getRepos = function(userName){
+  $.get('https://api.github.com/users/' + 'userName' + '?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
+exports.gituserModule = GitUser;
